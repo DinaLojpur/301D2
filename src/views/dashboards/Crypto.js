3a0d46948be5b1fe9fd1c3bb851493ac78659981
Riley McCarron <riley.mccarron@live.ca>
2023-10-16 23:12:17 -0400

    removed empty components from landing page

--- src/views/dashboards/Crypto.js
+++ src/views/dashboards/Crypto.js
@@ -1,11 +1,11 @@
-import { Row, Col } from 'reactstrap';
+// import { Row, Col } from 'reactstrap';
 import CryptoTopCards from '../../components/dashboard/cryptoDashboard/CryptoTopCards';
 import CryptoChart from '../../components/dashboard/cryptoDashboard/CryptoChart';
-import CryptoExchange from '../../components/dashboard/cryptoDashboard/CryptoExchange';
-import CryptoOrder from '../../components/dashboard/cryptoDashboard/CryptoOrder';
-import CryptoTable from '../../components/dashboard/cryptoDashboard/CryptoTable';
-import CryptoTrade from '../../components/dashboard/cryptoDashboard/CryptoTrade';
-import CryptoBuySell from '../../components/dashboard/cryptoDashboard/CryptoBuySell';
+// import CryptoExchange from '../../components/dashboard/cryptoDashboard/CryptoExchange';
+// import CryptoOrder from '../../components/dashboard/cryptoDashboard/CryptoOrder';
+// import CryptoTable from '../../components/dashboard/cryptoDashboard/CryptoTable';
+//import CryptoTrade from '../../components/dashboard/cryptoDashboard/CryptoTrade';
+//import CryptoBuySell from '../../components/dashboard/cryptoDashboard/CryptoBuySell';
 
 const Crypto = () => {
   return (
@@ -15,29 +15,29 @@ const Crypto = () => {
       {/********************* Crypto Chart ************************/}
       <CryptoChart />
       {/********************* Crypto Exchange & Order ************************/}
-      <Row>
+      {/* <Row>
         <Col lg="6">
           <CryptoExchange />
         </Col>
         <Col lg="6">
           <CryptoOrder />
         </Col>
-      </Row>
+      </Row> */}
       {/********************* Crypto Table ************************/}
-      <Row>
+      {/* <Row>
         <Col lg="12">
           <CryptoTable />
         </Col>
-      </Row>
+      </Row> */}
       {/********************* Crypto Trade & Buy Sell ************************/}
-      <Row>
+      {/* <Row>
         <Col lg="6">
           <CryptoTrade />
         </Col>
         <Col lg="6">
           <CryptoBuySell />
         </Col>
-      </Row>
+      </Row> */}
     </>
   );
 };

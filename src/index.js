f12896db4844a9b76fcbf9c408bd0fe6996ba059
amolui <akch20113@gmail.com>
2024-01-25 22:18:09 -0500

    Eslint error fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
+/*eslint-disable import/no-named-as-default-member; 
+eslint-disable import/no-named-as-default; */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';

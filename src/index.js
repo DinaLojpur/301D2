415669eb7b60c1f4a1727c6443f2a4c8b5e610a6
amolui <akch20113@gmail.com>
2024-01-26 16:05:54 -0500

    Removed react from eslint.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
